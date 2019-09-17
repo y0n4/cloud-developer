603f6e3fce2500fa96e9236ae76fcbc3b59a3924
y0n4 <04l.yona@gmail.com>
2019-09-16 22:05:50 -0400

    Write function to get all todos from backend

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
@@ -1,13 +1,30 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import * as AWS  from 'aws-sdk'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+const todosTable = process.env.TODOS_TABLE
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   // TODO: Get all TODO items for a current user
-  console.log(event);
-  // return {
-  //   statusCode: 200,
-  //   body: JSON.stringify('hello'),
-  // }
-  return undefined;
+  console.log('🌸', event);
+  
+  const result = await docClient.scan({
+    TableName: todosTable
+  }).promise()
+
+  const items = result.Items;
+
+  const response = {
+    statusCode: 200,
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({ items })
+  };
+
+  return response;
+  // return undefined;
 }
